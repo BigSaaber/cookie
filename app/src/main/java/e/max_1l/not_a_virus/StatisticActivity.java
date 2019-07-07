97831dc93aff0bd5149aa84a76cdd7df54766162
Maxim Movshin <maxim-wot-maxim@yandex.ru>
2019-07-07 22:52:26 +0300

    first normal change
    hah

--- app/src/main/java/e/max_1l/not_a_virus/StatisticActivity.java
+++ app/src/main/java/e/max_1l/not_a_virus/StatisticActivity.java
@@ -1,28 +1,36 @@
 package e.max_1l.not_a_virus;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class StatisticActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView allcookies, clickcookies, farmcookies ;
-    int allc, num, clickc, farmc ;
+    TextView allcookies, clickcookies, farmcookies,ach1name, ach1cond ;
+    ImageView ach1;
+    int allc, num, clickc, farmc,flag=0 ;
     Button back ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
-
+        ach1 = findViewById(R.id.ach1);
+        ach1name = findViewById(R.id.ach1name);
+        ach1cond = findViewById(R.id.ach1cond);
         allcookies = findViewById(R.id.allcookies) ;
         clickcookies = findViewById(R.id.clickcookies) ;
         farmcookies = findViewById(R.id.farmcookies) ;
         back = findViewById(R.id.back) ;
 
         back.setOnClickListener(this);
+        ach1.setOnClickListener(this);
 
         allc = getIntent().getIntExtra("allc", -1 ) ;
         num = getIntent().getIntExtra("num", -1 ) ;
@@ -32,8 +40,16 @@ public class StatisticActivity extends AppCompatActivity implements View.OnClick
         allcookies.setText(allc+" cookies produced");
         clickcookies.setText(clickc+" cookies by clicks");
         farmcookies.setText(farmc+" cookies by farm");
-    }
 
+        if (allc>=1000){
+            ach1.setAlpha((float)1);
+            ach1name.setAlpha((float)1);
+            ach1cond.setAlpha((float)1);
+        }
+
+
+
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -45,6 +61,7 @@ public class StatisticActivity extends AppCompatActivity implements View.OnClick
                 g.putExtra("farmc", farmc) ;
                 setResult(RESULT_OK,g);
                 finish();
+                break;
         }
     }
 }
